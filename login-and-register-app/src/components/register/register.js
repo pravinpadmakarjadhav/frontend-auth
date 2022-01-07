fc90528dd4989051b26c3e98429b593e7e287a5f
Pravin Jadhav <pr.jadhav02@gmail.com>
2022-01-07 15:38:29 +0530

    Update register.js

--- login-and-register-app/src/components/register/register.js
+++ login-and-register-app/src/components/register/register.js
@@ -28,8 +28,8 @@ const handleChange =e =>{
 const register =()=>{
     const {name,email,password,reEnterPassword} =user
     if (name && email && password && (password === reEnterPassword)){
-        alert('posted')
-       //axios.post("http://localhost:9002/register",user)
+       axios.post("http://localhost:9002/register",user)
+       .then(res => console.log(res))
     }else{
         alert ('invalid input')
     }
